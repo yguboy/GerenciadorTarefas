d1322f5c7fb810223267fd9741b480ed10592f81
Ygor Espada <ygorespada14@gmail.com>
2024-06-23 17:00:29 -0300

    Mais correções do front

--- API/GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
+++ API/GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aeb4dbac22e66c6a4b59bd1f315abad4d9aadb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4497ea90edf448c42cf93dd1876bca54faae6401")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
